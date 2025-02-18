a9909a4b23acc0ec8f61a84a837a9a5470282324
wachiraphongt <61167237+wachiraphongt@users.noreply.github.com>
2025-02-18 22:14:15 +0700

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 const Home = () => {
-  return <h1 className="h1-bold">Welcome to the world of Next.js</h1>;
+  return (
+    <div>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of Next.js
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
