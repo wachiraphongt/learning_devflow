6cb20df5b68f57defa2569f51139f6f983027ba8
wachiraphongt <61167237+wachiraphongt@users.noreply.github.com>
2025-05-05 22:08:57 +0700

    Implement User by Email

---
+++ app/api/users/[id]/route.ts
@@ -0,0 +1,76 @@
+import { NextResponse } from "next/server";
+
+import User from "@/database/user.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+
+export async function GET(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("User");
+
+  try {
+    await dbConnect();
+    const user = await User.findById(id);
+
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+// DELETE
+export async function DELETE(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("User");
+
+  try {
+    await dbConnect();
+    const user = await User.findByIdAndDelete(id);
+
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json({ success: true, data: user }, { status: 204 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+// PUT
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("User");
+
+  try {
+    await dbConnect();
+
+    const body = await request.json();
+    const validaedData = UserSchema.partial().parse(body);
+
+    const updatedUser = await User.findByIdAndUpdate(id, validaedData, {
+      new: true,
+    });
+
+    if (!updatedUser) throw new NotFoundError("User");
+
+    return NextResponse.json(
+      { success: true, data: updatedUser },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
---
+++ app/api/users/email/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+
+import User from "@/database/user.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+
+export async function POST(request: Request) {
+  const { email } = await request.json();
+
+  try {
+    const validatedData = UserSchema.partial().safeParse({ email });
+    if (!validatedData.success)
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+
+    await dbConnect();
+    const user = await User.findOne({ email });
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
