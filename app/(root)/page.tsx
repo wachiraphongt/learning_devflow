448671195565b0151285b82039cdb8d1ebdc8ca3
wachiraphongt <61167237+wachiraphongt@users.noreply.github.com>
2025-05-08 21:59:16 +0700

    Implement OAuth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
 
+import { auth } from "@/auth";
 import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-import { api } from "@/lib/api";
-import handleError from "@/lib/handlers/error";
 
 const questions = [
   {
@@ -49,27 +48,13 @@ const questions = [
   },
 ];
 
-const test = async () => {
-  try {
-    // await dbConnect();
-    // throw new ValidationError({
-    //   title: ["Required"],
-    //   tags: ['"Javascript" is not a valid tag'],
-    // });
-    return await api.users.getAll();
-  } catch (error) {
-    return handleError(error);
-  }
-};
-
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  const users = await test();
-
-  console.log(users);
+  const session = await auth();
+  console.log("Session: ", session);
 
   const { query = "", filter = "" } = await searchParams;
 
--- lib/validations.ts
+++ lib/validations.ts
@@ -109,3 +109,20 @@ export const AccountSchema = z.object({
     .string()
     .min(1, { message: "Provider Account ID is required." }),
 });
+
+export const SignInWithOAuthSchema = z.object({
+  provider: z.enum(["google", "github"]),
+  providerAccountId: z
+    .string()
+    .min(1, { message: "Provider Account ID is required." }),
+  user: z.object({
+    name: z.string().min(1, { message: "Name is required." }),
+    username: z
+      .string()
+      .min(3, { message: "Username must be at least 3 characters long." }),
+    email: z
+      .string()
+      .email({ message: "Please provide a valid email address." }),
+    image: z.string().url({ message: "Invalid image URL." }).optional(),
+  }),
+});
